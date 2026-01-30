de6f0aa39e9507df5c6b587a11f4e55a648948dc
Jacob <jacoblittler2003@gmail.com>
2026-01-29 19:31:41 -0500

    User migration-init and table created User validates name, email, and username.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :email, presence: true, length: { minimum: 6 }
+  validates :username, presence: true, length: { in: 6..20 }
 end
